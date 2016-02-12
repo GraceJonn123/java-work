331eb484ba29c93c1778f45d09584c337e2c0cd6
GraceJonn123 <gj.gjonn@gmail.com>
2016-02-12 18:33:16 +0300

    all files

---
+++ trial.java
@@ -0,0 +1,10 @@
+public class removeDuplicate{
+	public static void main(String[] args) {
+		String str = "Hello, i am here";
+		//adding a sentence
+		char[] y = str.toCharArray();
+//forming an array of different strings...h, e, l,l,etc.
+		for
+
+	}
+}
